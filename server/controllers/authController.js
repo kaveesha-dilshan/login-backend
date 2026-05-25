e2c193be86de26279fa008b1272ce66008ab04fb
kaveesha-dilshan <kaveeshadilshan5445@gmail.com>
2026-05-25 22:58:39 +0530

    add models and controllers

---
+++ server/controllers/authController.js
@@ -0,0 +1,36 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
+
+const reigsterUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({
+            message: "User registered successfully",
+            user
+        });
+
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
